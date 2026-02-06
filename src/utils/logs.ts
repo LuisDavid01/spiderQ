41f3b6d89004228ca9329e411fdeec74af8f419c
LuisDavid01 <luisdavid01@proton.me>
2026-02-06 17:17:04 -0600

    feat: fix if logs folder doesnt exist create it

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,9 +1,12 @@
 import fs from 'node:fs/promises'
 export async function logErrorLocal(message: string) {
+
+	await fs.mkdir(`${process.cwd()}/logs`, { recursive: true })
+
 	const file = await fs.open(`${process.cwd()}/logs/errors.txt`, 'a')
 
 	try {
-		await fs.writeFile(file, `${message}\n`)
+		await fs.writeFile(file, `${new Date().toISOString()}: ${message}\n`)
 	} catch (e) {
 	} finally {
 		await file.close()
