2b79b1fff0c2cf624f54b5c8c9930063f8ed5e01
LuisDavid01 <luisdavidmirandav12@gmail.com>
2026-02-18 22:19:21 -0600

    feat: test command

--- src/__tests__/command.test.ts
+++ src/__tests__/command.test.ts
@@ -3,7 +3,6 @@ import { test, expect, describe } from 'vitest'
 
 test('Execute command', async () => {
 	const result = await executeCommand('sudo -n nmap', '-sV -p 80 127.0.0.1', { timeout: 100000 })
-	console.log(result)
 	expect(result.success).toBe(true)
 
 }, 100000)
